24295a25c151e42ff14bad47bfa4872eceb86c07
Nursing Citizen Development <lincoln@clinyqai.com>
2025-12-26 15:19:59 +0000

    Deploy: Update site content, preserve CNAME, add .nojekyll

--- docs/assets/js/prism-fhirpath.js
+++ docs/assets/js/prism-fhirpath.js
@@ -54,11 +54,11 @@ Prism.languages.fhirpath = {
 	],
 	'variable': [
 		/(%\w+)\b/,
-		/(%`(?:[0-9A-TV-Za-tv-z_]|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|[ \-\."\\\/fnrt])+`)/ // this isn;t quite right, but it's a start
+		/(%`(?:\w|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|[ \-\."\\\/fnrt])+`)/ // this isn;t quite right, but it's a start
 	],
 	'identifier': [
 		{
-			pattern: /`(?:[0-9A-TV-Za-tv-z_]|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|[ \-\."\\\/fnrt])+`/,
+			pattern: /`(?:\w|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|[ \-\."\\\/fnrt])+`/,
 			// lookbehind: true,
 			greedy: true
 		},
